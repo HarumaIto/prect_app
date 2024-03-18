af0641544cd8bc1c2351c6b896d4ef762ed56108
HarumaIto <itoharm@gmail.com>
2024-03-19 00:03:24 +0900

    add bleの検索、スキャン機能を実装

--- lib/ui/setup/ble_setup_page.dart
+++ lib/ui/setup/ble_setup_page.dart
@@ -6,7 +6,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:prect/ui/components/default_snackbar.dart';
 
-class BleSetupPage extends ConsumerWidget {
+class BleSetupPage extends HookConsumerWidget {
   const BleSetupPage({Key? key}) : super(key: key);
 
   void bleInit(BuildContext context) async {
@@ -33,11 +33,63 @@ class BleSetupPage extends ConsumerWidget {
 
     subscription.cancel();
 
-    DefaultSnackbar.show(context, 'Bluetoothを有効化しました');
+    DefaultSnackbar.show(context, 'Bluetoothを有効化されています');
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final device = useState<BluetoothDevice?>(null);
+
+    void bleScan() async {
+      var subscription = FlutterBluePlus.onScanResults.listen(
+        (results) {
+          if (results.isNotEmpty) {
+            ScanResult r = results.last; // the most recently found device
+            device.value = r.device;
+            debugPrint(
+                '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+          }
+        },
+        onError: (e) => debugPrint(e),
+      );
+
+      FlutterBluePlus.cancelWhenScanComplete(subscription);
+
+      await FlutterBluePlus.adapterState
+          .where((val) => val == BluetoothAdapterState.on)
+          .first;
+
+      await FlutterBluePlus.startScan(
+        withServices: [Guid('2222')],
+        timeout: const Duration(seconds: 15),
+      );
+
+      await FlutterBluePlus.isScanning.where((val) => val == false).first;
+    }
+
+    void bleConnect() async {
+      // listen for disconnection
+      var subscription = device.value!.connectionState
+          .listen((BluetoothConnectionState state) async {
+        if (state == BluetoothConnectionState.disconnected) {
+          // 1. typically, start a periodic timer that tries to
+          //    reconnect, or just call connect() again right now
+          // 2. you must always re-discover services after disconnection!
+          debugPrint(
+              '${device.value!.disconnectReason?.code} ${device.value!.disconnectReason?.description}');
+        }
+      });
+
+      device.value!
+          .cancelWhenDisconnected(subscription, delayed: true, next: true);
+
+      await device.value!.connect();
+
+      subscription.cancel();
+
+      DefaultSnackbar.show(context, '接続しました');
+    }
+
     useEffect(() {
       bleInit(context);
       return null;
@@ -53,39 +105,12 @@ class BleSetupPage extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () async {},
-              child: const Text('Bluetoothを有効化する'),
+              onPressed: bleScan,
+              child: const Text('Bluetoothデバイスを検索する'),
             ),
             ElevatedButton(
-              onPressed: () async {
-                var subscription = FlutterBluePlus.onScanResults.listen(
-                  (results) {
-                    if (results.isNotEmpty) {
-                      ScanResult r =
-                          results.last; // the most recently found device
-                      debugPrint(
-                          '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
-                    }
-                  },
-                  onError: (e) => debugPrint(e),
-                );
-
-                FlutterBluePlus.cancelWhenScanComplete(subscription);
-
-                await FlutterBluePlus.adapterState
-                    .where((val) => val == BluetoothAdapterState.on)
-                    .first;
-
-                await FlutterBluePlus.startScan(
-                  withServices: [Guid('2222')],
-                  timeout: const Duration(seconds: 15),
-                );
-
-                await FlutterBluePlus.isScanning
-                    .where((val) => val == false)
-                    .first;
-              },
-              child: const Text('Bluetoothデバイスを検索する'),
+              onPressed: device.value != null ? bleConnect : null,
+              child: const Text('接続する'),
             ),
           ],
         ),
