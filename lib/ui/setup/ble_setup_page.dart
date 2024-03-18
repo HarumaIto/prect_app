77687790ac0d6b0cc508c163259bae805adf5e06
HarumaIto <itoharm@gmail.com>
2024-03-18 21:27:27 +0900

    add bleの有効化とスキャンのテストの実装

--- lib/ui/setup/ble_setup_page.dart
+++ lib/ui/setup/ble_setup_page.dart
@@ -13,36 +13,75 @@ class BleSetupPage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('BLEセットアップ'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () async {
-              // first, check if bluetooth is supported by your hardware
-              // Note: The platform is initialized on the first call to any FlutterBluePlus method.
-              if (await FlutterBluePlus.isSupported == false) {
-                print("Bluetooth not supported by this device");
-                return;
-              }
-
-              var subscription = FlutterBluePlus.adapterState
-                  .listen((BluetoothAdapterState state) {
-                print(state);
-                if (state == BluetoothAdapterState.on) {
-                  // usually start scanning, connecting, etc
-                } else {
-                  // show an error to the user, etc
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                // first, check if bluetooth is supported by your hardware
+                // Note: The platform is initialized on the first call to any FlutterBluePlus method.
+                if (await FlutterBluePlus.isSupported == false) {
+                  print("Bluetooth not supported by this device");
+                  return;
                 }
-              });
 
-              if (Platform.isAndroid) {
-                await FlutterBluePlus.turnOn();
-              }
+                var subscription = FlutterBluePlus.adapterState
+                    .listen((BluetoothAdapterState state) {
+                  print(state);
+                  if (state == BluetoothAdapterState.on) {
+                    // usually start scanning, connecting, etc
+                  } else {
+                    // show an error to the user, etc
+                  }
+                });
 
-              subscription.cancel();
-            },
-            child: const Text('Bluetoothを有効化する'),
-          ),
-        ],
+                if (Platform.isAndroid) {
+                  await FlutterBluePlus.turnOn();
+                }
+
+                subscription.cancel();
+
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Bluetoothを有効化しました'),
+                ));
+              },
+              child: const Text('Bluetoothを有効化する'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                var subscription = FlutterBluePlus.onScanResults.listen(
+                  (results) {
+                    if (results.isNotEmpty) {
+                      ScanResult r =
+                          results.last; // the most recently found device
+                      print(
+                          '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+                    }
+                  },
+                  onError: (e) => print(e),
+                );
+
+                FlutterBluePlus.cancelWhenScanComplete(subscription);
+
+                await FlutterBluePlus.adapterState
+                    .where((val) => val == BluetoothAdapterState.on)
+                    .first;
+
+                await FlutterBluePlus.startScan(
+                    withServices: [Guid("180D")],
+                    withNames: ["Bluno"],
+                    timeout: Duration(seconds: 15));
+
+                await FlutterBluePlus.isScanning
+                    .where((val) => val == false)
+                    .first;
+              },
+              child: const Text('Bluetoothデバイスを検索する'),
+            ),
+          ],
+        ),
       ),
     );
   }
