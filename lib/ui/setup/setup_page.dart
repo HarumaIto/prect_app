10e15ce052738b180bc9f2f2f90613f0d069dbc1
HarumaIto <itoharm@gmail.com>
2024-03-21 23:22:36 +0900

    update Navigator修正

--- lib/ui/setup/setup_page.dart
+++ lib/ui/setup/setup_page.dart
@@ -99,11 +99,12 @@ class SetupPage extends HookConsumerWidget {
                     ),
                   );
                 } else {
-                  Navigator.push(
+                  Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(
                       builder: (context) => const MainPage(),
                     ),
+                    (_) => false,
                   );
                 }
               },
